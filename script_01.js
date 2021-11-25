b3057fb034f4de6c10b64ba1655361e07506a814
Peter <schmidt.multimedia@gmail.com>
2021-11-25 09:33:16 +0100

    update lösung

--- script_01.js
+++ script_01.js
@@ -12,13 +12,39 @@
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-// output(getSentence(["Ich","bin","Max"],"S"));
-// output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin","Max"],"S"));
+output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin"],"E"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,"," habbe","Sie","die"],"Q"));
+function getSentence(arr,op) {
 
-function getSentence(arr,op){
-	
-	// ....
-	
+    const gap = " ";
+    const punct = getPunct(op);
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if ( i != arr.length-1 ) {
+            str += arr[i] + gap; 
+        } else {
+            str += arr[i] + punct; 
+        }
+    }
+
+    return str;
+}
+
+function getPunct(op){
+    switch (op) {
+        case "S":
+            return ".";
+        case "Q":
+            return "?";
+        case "E":
+            return "!";
+        default:
+            return ".";  // TBA
+    }
 }
 
 // Modul: Ausgabe in Konsole : Test
